7ff126a6c5d28dfc25afc35f8037c16def2092c5
ZuHuitzila <73566932+ZuHuitzila@users.noreply.github.com>
2021-07-01 02:32:53 -0500

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://a4d3ab14.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
